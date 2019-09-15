1c7315f004c457224d10628e3b3b2ac4cefb21c5
Francis Distor <francis.distor@gmail.com>
2019-09-15 14:43:42 -0700

    add routes and use router

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 
 const app = require('express')();
-const router = require('./router.js.js');
+const router = require('./router.js');
 const bodyParser = require('body-parser');
 const port = process.env.PORT || 8000;
 
@@ -9,6 +9,10 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.get('/vehicles/:id', router.getVehicleInfo);
+app.get('/vehicles/:id/doors', router.getSecurity);
+app.get('/vehicles/:id/fuel', router.getFuel);
+app.get('/vehicles/:id/battery', router.getBattery);
+app.post('/vehicles:/id/engine', router.startEngine);
 
 app.listen(port, err => {
   if (err) console.error(err);
