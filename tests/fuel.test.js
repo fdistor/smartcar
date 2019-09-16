79cae8260b033d835ff2f4729a3ee7c4a859daed
Francis Distor <francis.distor@gmail.com>
2019-09-16 02:04:03 -0700

    cp other tests here

---
+++ tests/fuel.test.js
@@ -0,0 +1,52 @@
+const { expect } = require('chai');
+const sinon = require('sinon');
+const router = require('../server/router');
+
+describe('Security endpoint', () => {
+  let res;
+
+  beforeEach(() => {
+    res = {};
+
+    res.status = sinon.stub().returns(res);
+    res.send = sinon.stub().returns(res);
+  });
+
+  afterEach(() => {
+    res.status.resetHistory();
+    res.send.resetHistory();
+  });
+
+  it('Should reformat data on valid vehicle id', async () => {
+    const req1234 = { params: { id: 1234 } };
+    const req1235 = { params: { id: 1235 } };
+
+    await router.getSecurity(req1234, res);
+
+    expect(res.status.getCall(0).args[0]).to.equal(200);
+    expect(res.send.getCall(0).args[0]).to.deep.equal({
+      percent: 84.15
+    });
+
+    res.status.resetHistory();
+    res.send.resetHistory();
+
+    await router.getSecurity(req1235, res);
+
+    expect(res.status.getCall(0).args[0]).to.equal(200);
+    expect(res.send.getCall(0).args[0]).to.deep.equal({
+      percent: null
+    });
+  });
+
+  it('Should return status 404 on invalid vehicle id', async () => {
+    const req = { params: { id: 1236 } };
+
+    await router.getSecurity(req, res);
+
+    expect(res.status.getCall(0).args[0]).to.equal(404);
+    expect(res.send.getCall(0).args[0]).to.deep.equal({
+      reason: 'Vehicle id: 1236 not found.'
+    });
+  });
+});
