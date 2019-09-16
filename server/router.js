fad243c98d3a18c9ae572bdf60d55f10df261188
Francis Distor <francis.distor@gmail.com>
2019-09-15 23:54:45 -0700

    complete getVehicleInfo

--- server/router.js
+++ server/router.js
@@ -1,8 +1,36 @@
+const GMApi = require('../mocks/GMApi.js');
+const gm = new GMApi();
+
 module.exports = {
   getVehicleInfo: async (req, res) => {
     const { id } = req.params;
-    const GMVehicleResult = await fs.readFile('../mocks/vehicleInfo.json');
+    const result = await gm.getVehicleInfo(id);
+    const parsed = JSON.parse(result);
+    const { status } = parsed;
 
-    res.status(200).send(GMVehicleResult);
-  }
+    if (status === '200') {
+      const { data } = parsed;
+      const color = data.color.value;
+      const driveTrain = data.driveTrain.value;
+      const vin = data.vin.value;
+      const doorCount = data.fourDoorSedan.value === 'True' ? 4 : 2;
+
+      res.status(200).send({
+        color,
+        driveTrain,
+        vin,
+        doorCount
+      });
+    } else {
+      res.send(parsed);
+    }
+  },
+
+  getSecurity: async (req, res) => {},
+
+  getFuel: async (req, res) => {},
+
+  getBattery: async (req, res) => {},
+
+  startEngine: async (req, res) => {}
 };
