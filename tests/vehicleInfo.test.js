bf8228f97619db52836f4f82d4cd4619132c11fe
Francis Distor <francis.distor@gmail.com>
2019-09-15 23:54:53 -0700

    finish testing

--- tests/vehicleInfo.test.js
+++ tests/vehicleInfo.test.js
@@ -1,18 +1,61 @@
 const { expect } = require('chai');
-const axios = require('axios');
-const url = 'http://localhost:8000';
+const sinon = require('sinon');
+const router = require('../server/router.js');
 
-describe('Vehicle endpoint', () => {
-  it('Should get vehicle info for valid id', async () => {
-    const response = await axios.get(url + '/vehicles/1234');
+describe('Vehicle Info endpoint', () => {
+  let res;
 
-    expect(response).to.deep.equal({
-      vin: '1213231',
-      color: 'Metallic Silver',
-      doorCount: 4,
-      driveTrain: 'v8'
-    });
+  beforeEach(() => {
+    res = {};
+
+    res.status = sinon.stub().returns(res);
+    res.send = sinon.stub().returns(res);
   });
 
-  it('Should return ');
+  it('Should reformat data on valid vehicle id', async () => {
+    const req1234 = { params: { id: 1234 } };
+    const req1235 = { params: { id: 1235 } };
+
+    const res1234 = { ...res };
+    const res1235 = { ...res };
+
+    await router.getVehicleInfo(req1234, res1234);
+    await router.getVehicleInfo(req1235, res1235);
+
+    expect(res1234.status.calledWith(200)).to.be.true;
+    expect(
+      res1234.send.calledWith({
+        vin: '123123412412',
+        color: 'Metallic Silver',
+        doorCount: 4,
+        driveTrain: 'v8'
+      })
+    ).to.be.true;
+
+    expect(res1235.status.calledWith(200)).to.be.true;
+    expect(
+      res1235.send.calledWith({
+        vin: '1235AZ91XP',
+        color: 'Forest Green',
+        doorCount: 2,
+        driveTrain: 'electric'
+      })
+    ).to.be.true;
+  });
+
+  it('Should return status 404 on invalid vehicle id', async () => {
+    const req = { params: { id: 1236 } };
+
+    res.reason = sinon.stub().returns(res);
+
+    await router.getVehicleInfo(req, res);
+
+    expect(res.status.calledWith(404));
+    expect(
+      res.reason.calledWith({
+        reason: 'Vehicle id: 1236 not found.',
+        status: 404
+      })
+    );
+  });
 });
