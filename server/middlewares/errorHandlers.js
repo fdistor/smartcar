7b0fcb7a6ba38ec96e33fc7af61ab5a9fd00c991
Francis Distor <francis.distor@gmail.com>
2019-09-17 12:38:42 -0700

    better handling

--- server/middlewares/errorHandlers.js
+++ server/middlewares/errorHandlers.js
@@ -2,9 +2,18 @@ const app = require('./routes.js');
 const { errorLogger } = require('../../logger/winston.js');
 
 app.use(errorLogger);
+
 app.use((error, req, res, next) => {
-  console.error(error, 'Promise failed (assuming from GM API).');
-  res.status(502).send({ message: 'Failed to fetch from GM API.' });
+  if (error.message === '1')
+    res
+      .status(502)
+      .send({ message: 'Bad gateway, failed to fetch from upstream server.' });
+  next(error);
+});
+
+app.use((error, req, res, next) => {
+  if (error.message === '0')
+    res.status(500).send({ message: 'Internal server error.' });
   next(error);
 });
 
