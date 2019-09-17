635a6a2da8a134c1bf75e0d0555ab29773688740
Francis Distor <francis.distor@gmail.com>
2019-09-17 11:58:05 -0700

    use loggers

--- server/index.js
+++ server/index.js
@@ -3,16 +3,20 @@ require('dotenv').config();
 const app = require('express')();
 const router = require('./router.js');
 const bodyParser = require('body-parser');
+const { combinedLogger, errorLogger } = require('../logger/winston.js');
 const port = process.env.PORT || 8000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(combinedLogger);
 
 app.get('/vehicles/:id', router.getVehicleInfo);
 app.get('/vehicles/:id/doors', router.getSecurity);
 app.get('/vehicles/:id/fuel', router.getFuel);
 app.get('/vehicles/:id/battery', router.getBattery);
 app.post('/vehicles/:id/engine', router.postEngine);
+
+app.use(errorLogger);
 
 app.use((error, req, res, next) => {
   console.error(error, 'Promise failed (assuming from GM API).');
